a95e22310d8609b1718d72214000c92e76f147fb
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-01-01 11:56:19 +0530

    Added: Demonstrating various file operations

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollService.java
@@ -1,31 +1,44 @@
 package com.bridgelabz.employeepayrollservice;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+    public enum IOService{CONSOLE_IO, FILE_IO, REST_IO}
     private List<EmployeePayrollData> employeePayrollList;
+
     public EmployeePayrollService() {
-        employeePayrollList = new ArrayList<>();
+        employeePayrollList = new ArrayList<EmployeePayrollData>();
     }
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Services Program");
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
         employeePayrollService.writeEmployeePayrollData();
-
     }
     private void readEmployeePayrollData(Scanner consoleInputReader) {
         System.out.println("Enter Employee ID: ");
         int id = consoleInputReader.nextInt();
-        System.out.println("Enter Employee Name: ");
+        System.out.println("Enter Employee name: ");
         String name = consoleInputReader.next();
         System.out.println("Enter Employee Salary: ");
         double salary = consoleInputReader.nextDouble();
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
     private void writeEmployeePayrollData() {
-        System.out.println("\nWriting Employee Payroll to console\n" + employeePayrollList);
+        System.out.println("\nWriting Employee Payroll Data to Console\n" + employeePayrollList);
+    }
+    public static boolean deleteFiles(File contentsToDelete) {
+        File[] allContents = contentsToDelete.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
     }
 }
---
+++ src/test/java/com/bridgelabz/employeepayrollservice/IOFileAPITest.java
@@ -0,0 +1,2 @@
+package com.bridgelabz.employeepayrollservice;public class IOFileAPITest {
+}
