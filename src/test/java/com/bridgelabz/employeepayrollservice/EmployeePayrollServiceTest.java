7d1c7c1ab44984780599ce8e33a9d14e87b4468a
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-01-01 13:24:08 +0530

    Added: Counting Employee Payroll Service Entries

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -7,12 +7,27 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmployeePayrollServiceTest {
+    /**
+     * UC4
+     */
     @Test
-    public void whenThreeEmployeeAddedShouldReturnCountOfEmployees() {
+    public void whenThreeEmployeeAddedShouldWriteToAFile() {
         EmployeePayrollData[] employeeList = { new EmployeePayrollData(1, "Rama", 50000),
-                new EmployeePayrollData(2, "Shyam", 50000), new EmployeePayrollData(3, "Krishna", 50000) };
+                new EmployeePayrollData(2, "Shyama", 50000), new EmployeePayrollData(3, "Krishna", 50000) };
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(employeeList));
         employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+    }
+
+    /**
+     * UC5
+     */
+    @Test
+    public void whenThreeEmployeeAddedShouldReturnCountOfEmployeesAndPrintData() {
+        EmployeePayrollData[] employeeList = { new EmployeePayrollData(1, "Rama", 50000),
+                new EmployeePayrollData(2, "Shyama", 50000), new EmployeePayrollData(3, "Krishna", 50000) };
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(employeeList));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
         employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
         long entries = employeePayrollService.countEntries();
         assertEquals(3, entries);
