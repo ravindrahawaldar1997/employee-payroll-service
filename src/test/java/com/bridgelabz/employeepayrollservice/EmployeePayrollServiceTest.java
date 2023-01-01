bb608532aface007dc89151cf6dbc948edb02d35
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2023-01-01 14:25:37 +0530

    Added: reading Employee Payroll files from Employee payroll Service program

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollservice;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,5 +32,18 @@ public class EmployeePayrollServiceTest {
         employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
         long entries = employeePayrollService.countEntries();
         assertEquals(3, entries);
+    }
+    /**
+     * UC6
+     */
+    @Test
+    public void whenReadFromFileShouldReturnListOfEmployees() {
+        EmployeePayrollData[] employeeList = {new EmployeePayrollData(1, "Rama", 50000),
+                new EmployeePayrollData(2, "Shyama", 50000), new EmployeePayrollData(3, "Krishna", 50000)};
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(employeeList));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        List<EmployeePayrollData> list = employeePayrollService.readData(EmployeePayrollService.IOService.FILE_IO);
+        list.forEach(System.out::println);
+        assertEquals(3, list.size());
     }
 }
